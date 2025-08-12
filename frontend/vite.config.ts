0f3de91e6f643ff1696ed6619c31c8a87d40ca56
Anuraag <anuraagbagal02@gmail.com>
2025-08-12 08:55:01 +0530

    deploy-push-4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,9 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  build: {
-    outDir: "dist"
-  },
+  
   server: {
     host: "::",
     port: 8081,
